dd74840fc45b3df1a9c7470d985714b996e69c59
zsevic <zeljkosevic95@gmail.com>
2022-07-04 23:07:37 +0200

    Improve code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 module.exports = async function App(context) {
   try {
-    const date = new Date(Date.now() - 1000 * 60);
+    const date = new Date();
+    const updatedDate = new Date();
+    updatedDate.setTime(date.getTime() - 60 * 1000);
     const params = {
       event_type: 'successful',
-      occurred_after: Math.floor(date.getTime() / 1000),
+      occurred_after: Math.floor(updatedDate.getTime() / 1000),
     };
     const url = 'https://api.opensea.io/api/v1/events';
     const headers = {
@@ -11,9 +13,11 @@ module.exports = async function App(context) {
     };
     let next;
     let results = {};
-    let counter = 0;
+    let requestNumber = 0;
+    let newItems = 0;
 
     do {
+      console.log(`Sending ${requestNumber+1}. request...`);
       const response = await fetch(
         `${url}?event_type=${params.event_type}&occurred_after=${params.occurred_after}`,
         {
@@ -21,32 +25,36 @@ module.exports = async function App(context) {
         }
       ).then((res) => res.json());
       next = response.next;
+      newItems = response.asset_events.length || 0;
       response.asset_events.forEach((event) => {
         const collectionName = event.asset?.collection?.name;
         if (!collectionName) return;
         if (results[collectionName]) {
-          return (results[collectionName].numberOfSales += 1);
+          return (results[collectionName].numberOfSales += Number(event.quantity));
         }
         return (results[collectionName] = {
           slug: event.asset?.collection?.slug,
-          numberOfSales: 1,
+          numberOfSales: Number(event.quantity),
         });
       });
-      counter += 1;
+      requestNumber += 1;
       console.log(
-        `Got ${response.asset_events.length} events, ${counter}. request`
+        `Got ${response.asset_events.length} events, ${requestNumber}. request`
       );
-    } while (next && counter <= 10);
+    } while (next && newItems !== 0 && requestNumber <= 10);
     const sorted = Object.entries(results).sort(
       (a, b) => b[1].numberOfSales - a[1].numberOfSales
     );
+    if (sorted.length === 0) {
+      return context.sendMessage('There are no trending collections');
+    }
     const response = sorted
       .map(
         (result) =>
           `<a href="https://opensea.io/collection/${result[1].slug}">${result[0]}</a>: ${result[1].numberOfSales}`
       )
       .join('\n');
-    await context.sendMessage(response, {
+    await context.sendMessage(`Trending after ${updatedDate.toLocaleTimeString()}\n${response}`, {
       parseMode: 'HTML',
     });
   } catch (error) {
