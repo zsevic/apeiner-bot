28429e16e2a617c8c6068d7735b38a9c473c9765
zsevic <zeljkosevic95@gmail.com>
2022-07-05 14:15:34 +0200

    Add timeout and timeframes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const protocols = ["ERC721", "ERC1155"];
+const PROTOCOLS = ["ERC721", "ERC1155"];
+const TIMEOUT = 3000;
 
 module.exports = async function App(context) {
   try {
+    const textMessage = context._requestContext?.body?.message?.text;
+    const minutes = [1, 2, 5, 10];
+    const isMinute = minutes.includes(Number(textMessage));
+    let seconds = 60;
+    if (isMinute) {
+      seconds = 60 * Number(textMessage);
+    }
+
     const date = new Date();
     const updatedDate = new Date();
-    updatedDate.setTime(date.getTime() - 60 * 1000);
+    updatedDate.setTime(date.getTime() - seconds * 1000);
     const params = {
       event_type: 'successful',
       occurred_after: Math.floor(updatedDate.getTime() / 1000),
@@ -25,14 +34,18 @@ module.exports = async function App(context) {
       };
       const url = new URL('https://api.opensea.io/api/v1/events');
       url.search = new URLSearchParams(queryParams);
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), TIMEOUT);
       const response = await fetch(url.toString(), {
         headers,
+        signal: controller.signal,
       }).then((res) => res.json());
+      clearTimeout(timeoutId);
       next = response.next;
       newItems = response.asset_events.length || 0;
       response.asset_events.forEach((event) => {
         const schemaNema = event.asset?.asset_contract?.schema_name;
-        if (!protocols.includes(schemaNema)) {
+        if (!PROTOCOLS.includes(schemaNema)) {
           console.log(`Skipping ${schemaNema}`);
           return;
         }
