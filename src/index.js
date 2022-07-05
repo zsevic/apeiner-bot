2251924178b97cc1b4e4ffe79f6a38b92d64e9b2
zsevic <zeljkosevic95@gmail.com>
2022-07-05 17:42:53 +0200

    Improve requests

--- src/index.js
+++ src/index.js
@@ -35,9 +35,8 @@ module.exports = async function App(context) {
         ...params,
         ...(next && { cursor: next }),
       };
-      const url = new URL('https://api.opensea.io/api/v1/events');
-      url.search = new URLSearchParams(queryParams);
-      const response = await axios(url.toString(), {
+      const response = await axios('https://api.opensea.io/api/v1/events', {
+        params: queryParams,
         headers,
         timeout: TIMEOUT,
       }).then((res) => res.data);
