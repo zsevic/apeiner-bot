f0f7cccda4c36aff2563ddea516836032b70d0a0
zsevic <zeljkosevic95@gmail.com>
2022-07-14 23:53:02 +0200

    Refactor

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-const bodyParser = require('body-parser');
-const express = require('express');
 const { bottender } = require('bottender');
+const { setupCustomServer } = require('./src/custom-server');
 
 const app = bottender({
   dev: process.env.NODE_ENV !== 'production',
 });
 
-const port = Number(process.env.PORT) || 5000;
-
-// the request handler of the bottender app
-const handle = app.getRequestHandler();
-
 app.prepare().then(() => {
-  const server = express();
-
-  const verify = (req, _, buf) => {
-    req.rawBody = buf.toString();
-  };
-  server.use(bodyParser.json({ verify }));
-  server.use(bodyParser.urlencoded({ extended: false, verify }));
-
-  // route for webhook request
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+  setupCustomServer(app);
+}).catch(console.error);
---
+++ src/custom-server.js
@@ -0,0 +1,31 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const setupCustomServer = (app) => {
+  const port = Number(process.env.PORT) || 5000;
+
+  // the request handler of the bottender app
+  const handle = app.getRequestHandler();
+
+  const server = express();
+
+  const verify = (req, _, buf) => {
+    req.rawBody = buf.toString();
+  };
+  server.use(bodyParser.json({ verify }));
+  server.use(bodyParser.urlencoded({ extended: false, verify }));
+
+  // route for webhook request
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+}
+
+module.exports = {
+  setupCustomServer,
+};
