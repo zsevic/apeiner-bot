bb4f9d1dab8d1b90039ec146e9ad4a3733710b51
zsevic <zeljkosevic95@gmail.com>
2022-09-01 02:02:00 +0200

    Deactivate scheduler

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { bottender } = require('bottender');
 const { setupCustomServer } = require('./src/custom-server');
-const { setupScheduler } = require('./src/scheduler');
+// const { setupScheduler } = require('./src/scheduler');
 
 const app = bottender({
   dev: process.env.NODE_ENV !== 'production',
@@ -10,8 +10,8 @@ app
   .prepare()
   .then(() => {
     setupCustomServer(app);
-    if (process.env.NODE_ENV !== 'dev') {
-      setupScheduler();
-    }
+    // if (process.env.NODE_ENV !== 'dev') {
+    //   setupScheduler();
+    // }
   })
   .catch(console.error);
