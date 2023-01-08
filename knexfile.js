e73c6afceec0d176049d651159fb485ca6057cf7
zsevic <zeljkosevic95@gmail.com>
2023-01-09 00:21:42 +0100

    Prepare production environment

--- knexfile.js
+++ knexfile.js
@@ -20,31 +20,13 @@ module.exports = {
     useNullAsDefault: true,
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      uri: process.env.DATABASE_URL,
     },
     pool: {
-      min: 2,
+      min: 1,
       max: 10,
     },
     migrations: {
