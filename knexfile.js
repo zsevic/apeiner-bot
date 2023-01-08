0521db59a64a35e8f895b2575b7d3241ec054761
zsevic <zeljkosevic95@gmail.com>
2023-01-09 00:26:21 +0100

    Fix database configuration for test environment

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,21 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  test: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      host: process.env.DATABASE_HOSTNAME,
+      port: process.env.DATABASE_PORT,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+    useNullAsDefault: true,
+  },
+
   production: {
     client: 'postgresql',
     connection: {
