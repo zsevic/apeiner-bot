2bacdc4ac3556edcbadce18f7a9fbec3d36ba181
zsevic <zeljkosevic95@gmail.com>
2023-01-10 23:42:19 +0100

    Refactor

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,23 +2,38 @@ const userRepository = require('../gateways/user-repository');
 const { getStatusMessage, getTime } = require('../utils');
 const service = require('./');
 
+/**
+ * 
+ * @param {*} context 
+ * @param {number} userId 
+ * @returns {Promise<string>}
+ */
+const handleNewUser = async (context, userId) => {
+  const newUser = {
+    id: userId,
+    username: context.event._rawEvent.message?.chat?.username,
+  };
+  await userRepository.saveUser(newUser);
+  const time = getTime('1');
+  const [, minutes] = time;
+  const statusMessage = getStatusMessage(minutes);
+  await context.sendMessage(statusMessage);
+  return service.getResponseMessage(...time, userId);
+}
+
+/**
+ * 
+ * @param {*} context 
+ * @returns {Promise<string>}
+ */
 const getResponseMessage = async (context) => {
   const userId = context.event._rawEvent.message?.chat?.id;
   const user = await userRepository.getUserById(userId);
   if (!user) {
-    const newUser = {
-      id: userId,
-      username: context.event._rawEvent.message?.chat?.username,
-    };
-    await userRepository.saveUser(newUser);
-    const time = getTime('1');
-    const [, minutes] = time;
-    const statusMessage = getStatusMessage(minutes);
-    await context.sendMessage(statusMessage);
-    return service.getResponseMessage(...time, userId);
+    return handleNewUser(context, userId);
   }
 
-  return 'In order to activate apeiner, send 0.05ETH to apeiner.eth\nAfter you complete the first step, send the message in the following format: \n/activate [YOUR WALLET ADDRESS]\nfor example: /activate 0x71C7656EC7ab88b098defB751B7401B5f6d8976F';
+  return 'In order to activate apeiner, send 0.05ETH to apeiner.eth\n\nAfter you complete the first step, send the message in the following format: \n/activate [YOUR WALLET ADDRESS]\n\nfor example: /activate 0x71C7656EC7ab88b098defB751B7401B5f6d8976F';
 };
 
 module.exports = {
