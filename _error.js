e3895366bb08af3ee11a2a1d6df26717c225f4d6
zsevic <zeljkosevic95@gmail.com>
2023-02-17 16:41:00 +0100

    Improve error handler

--- _error.js
+++ _error.js
@@ -1,4 +1,7 @@
+const { getClient } = require('bottender');
+const { CHAT_ID } = require('./src/constants');
 const { logger } = require('./src/logger');
+const client = getClient('telegram');
 
 module.exports = async (context, props) => {
   logger.error(props.error);
@@ -6,4 +9,8 @@ module.exports = async (context, props) => {
   await context.sendMessage(
     'There are some unexpected errors that happened. Please try again later. Sorry for the inconvenience.'
   );
+
+  if (process.env.NODE_ENV === 'production') {
+    client.sendMessage(CHAT_ID, props.error.stack);
+  }
 };
