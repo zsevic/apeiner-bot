c6d44e94aa92d86e483dfe063193af638b31dfe2
zsevic <zeljkosevic95@gmail.com>
2023-02-17 17:17:54 +0100

    Configure Sentry

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const Sentry = require('@sentry/node');
 const { bottender } = require('bottender');
 const ngrok = require('ngrok');
 const shell = require('shelljs');
 const { setupCustomServer } = require('./src/custom-server');
 const { logger } = require('./src/logger');
 const { setupScheduler } = require('./src/scheduler');
+
+Sentry.init({
+  dsn: "https://2ea1109ee389439289f63a2074d9ba86@o390513.ingest.sentry.io/4504696281628672",
+});
 
 const app = bottender({
   dev: process.env.NODE_ENV !== 'production',
