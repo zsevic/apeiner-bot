588a0c07684011b7162a624ffbba0fb19658765e
zsevic <zeljkosevic95@gmail.com>
2023-02-17 13:17:56 +0100

    Add tests

--- src/handlers/message-handler.test.js
+++ src/handlers/message-handler.test.js
@@ -4,6 +4,24 @@ const nftApi = require('../gateways/nft-api');
 const userRepository = require('../gateways/user-repository');
 const utils = require('../utils');
 
+const firstGetEventsResponse = require('./mocks/data/get-events-1.json');
+
+const firstGetCollectionInfoResponse = require('./mocks/data/get-collection-info-1.json');
+const secondGetCollectionInfoResponse = require('./mocks/data/get-collection-info-2.json');
+const thirdGetCollectionInfoResponse = require('./mocks/data/get-collection-info-3.json');
+const fourthGetCollectionInfoResponse = require('./mocks/data/get-collection-info-4.json');
+const fifthGetCollectionInfoResponse = require('./mocks/data/get-collection-info-5.json');
+const sixthGetCollectionInfoResponse = require('./mocks/data/get-collection-info-6.json');
+const seventhGetCollectionInfoResponse = require('./mocks/data/get-collection-info-7.json');
+
+const firstGetAssetEventsResponse = require('./mocks/data/get-asset-events-1.json');
+const secondGetAssetEventsResponse = require('./mocks/data/get-asset-events-2.json');
+const thirdGetAssetEventsResponse = require('./mocks/data/get-asset-events-3.json');
+
+const firstGetCollectionStatsResponse = require('./mocks/data/get-collection-stats-1.json');
+const secondGetCollectionStatsResponse = require('./mocks/data/get-collection-stats-2.json');
+const thirdGetCollectionStatsResponse = require('./mocks/data/get-collection-stats-3.json');
+
 describe('HandleMessage', () => {
   describe('/users command', () => {
     it('should return error message command is not valid', async () => {
@@ -201,6 +219,64 @@ describe('HandleMessage', () => {
       expect(context.sendMessage).toHaveBeenNthCalledWith(
         2,
         'There are no bought NFTs in last 1 minute (after 1:00:00 AM)',
+        defaultAdminReply
+      );
+    });
+
+    it('should return results', async () => {
+      const context = {
+        event: {
+          text: '/1',
+          _rawEvent: {
+            message: {
+              entities: [
+                {
+                  type: 'bot_command',
+                },
+              ],
+              chat: {
+                id: CHAT_ID,
+              },
+            },
+          },
+        },
+        sendMessage: jest.fn(),
+      };
+
+      jest
+        .spyOn(nftApi, 'getEvents')
+        .mockResolvedValueOnce(firstGetEventsResponse);
+      jest
+        .spyOn(nftApi, 'getCollectionInfo')
+        .mockResolvedValueOnce(firstGetCollectionInfoResponse)
+        .mockResolvedValueOnce(secondGetCollectionInfoResponse)
+        .mockResolvedValueOnce(thirdGetCollectionInfoResponse)
+        .mockResolvedValueOnce(fourthGetCollectionInfoResponse)
+        .mockResolvedValueOnce(fifthGetCollectionInfoResponse)
+        .mockResolvedValueOnce(sixthGetCollectionInfoResponse)
+        .mockResolvedValueOnce(seventhGetCollectionInfoResponse);
+      jest
+        .spyOn(nftApi, 'getAssetEvents')
+        .mockResolvedValueOnce(firstGetAssetEventsResponse)
+        .mockResolvedValueOnce(secondGetAssetEventsResponse)
+        .mockResolvedValueOnce(thirdGetAssetEventsResponse);
+      jest
+        .spyOn(nftApi, 'getCollectionStats')
+        .mockResolvedValueOnce(firstGetCollectionStatsResponse)
+        .mockResolvedValueOnce(secondGetCollectionStatsResponse)
+        .mockResolvedValueOnce(thirdGetCollectionStatsResponse);
+
+      jest.spyOn(utils, 'getDate').mockReturnValue(new Date('2023-02-17'));
+
+      await HandleMessage(context);
+
+      expect(context.sendMessage).toHaveBeenNthCalledWith(
+        1,
+        'Getting stats for last 1 minute...'
+      );
+      expect(context.sendMessage).toHaveBeenNthCalledWith(
+        2,
+        `Bought NFTs in last 1 minute (after 1:00:00 AM)\n\n&#x2713; <a href="https://gem.xyz/collection/thememes6529">The Memes by 6529</a>: 3 sales\nunique buyers: 1\nunique sellers: 3\nsold for 0.22 - 0.224eth\nfloor: 0.092eth\nbest offer: 0.08weth\none hour average price: 0.091eth\naverage price: 0.12eth\none hour sales: 17\ntotal sales: 9350\ntotal volume: 1120.4eth\nlisted/supply: 187/2000\nowners/supply: 1063/2000\nroyalty: 0%\ncreation date: 24 November 2022\n<a href="https://twitter.com/cryptobirbsnft">twitter</a>\n<a href="https://coniun.io/collection/thememes6529/dashboard">dashboard</a>\n\n<a href="https://gem.xyz/collection/emblem-vault">Emblem Vault [Ethereum]</a>: 1 sale\nsold for 1.15eth\nfloor: 0.58eth\nbest offer: 1.122weth\none hour average price: 1.229eth\naverage price: 0.878eth\none hour sales: 12\ntotal sales: 3631\ntotal volume: 3186.8eth\nlisted/supply: 579/5611\nowners/supply: 1804/5611\nroyalty: 0%\ncreation date: 12 February 2023\n<a href="https://twitter.com/jackbutcher">twitter</a>\n<a href="https://coniun.io/collection/emblem-vault/dashboard">dashboard</a>\n\n&#x2713; <a href="https://gem.xyz/collection/vv-checks-originals">Checks - VV Originals</a>: 1 sale\nsold for 3.9eth\nfloor: 0.011eth\nbest offer: 0.008weth\none hour average price: 0.023eth\naverage price: 0.097eth\ntotal sales: 13571\ntotal volume: 1318.2eth\nlisted/supply: 213/7777\nowners/supply: 1614/7777\nroyalty: 7.5%\ncreation date: 31 March 2022\n<a href="https://twitter.com/DreadfulzNFT">twitter</a>\n<a href="https://coniun.io/collection/vv-checks-originals/dashboard">dashboard</a>\n`,
         defaultAdminReply
       );
     });
