b391c204b539bacaf554d2529010323e372d655f
zsevic <zeljkosevic95@gmail.com>
2023-02-17 10:02:18 +0100

    Initial tests

---
+++ src/handlers/message-handler.test.js
@@ -0,0 +1,31 @@
+const { CHAT_ID, replyMarkup } = require('../constants');
+const { HandleMessage } = require('./message-handler');
+
+describe('HandleMessage', () => {
+  describe('/users command', () => {
+    it('should return response command is not valid', async () => {
+      const context = {
+        event: {
+          text: '/users',
+          _rawEvent: {
+            message: {
+              entities: [{
+                type: 'bot_command',
+              }],
+              chat: {
+                id: CHAT_ID,
+              },
+            },
+          },
+        },
+        sendMessage: jest.fn(),
+      };
+      await HandleMessage(context);
+  
+      expect(context.sendMessage).toBeCalledWith('Command is not valid', {
+        parseMode: 'HTML',
+        replyMarkup,
+      });
+    });  
+  });
+});
